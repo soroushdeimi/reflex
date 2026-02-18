335217a03114e0092ed0da4326632e6b1218fa1b
nima1313 <nimaalighardashi@gmail.com>
2026-02-18 22:12:00 +0330

    feat: implement ReadFrame and WriteFrame methods in Session structure

--- xray-core/proxy/reflex/session.go
+++ xray-core/proxy/reflex/session.go
@@ -2,6 +2,8 @@ package reflex
 
 import (
 	"crypto/cipher"
+	"encoding/binary"
+	"io"
 
 	"golang.org/x/crypto/chacha20poly1305"
 )
@@ -25,4 +27,56 @@ func NewSession(sessionKey []byte) (*Session, error) {
 		readNonce:  0,
 		writeNonce: 0,
 	}, nil
+}
+
+func (s *Session) ReadFrame(reader io.Reader) (*Frame, error) {
+	header := make([]byte, 3)
+	if _, err := io.ReadFull(reader, header); err != nil {
+		return nil, err
+	}
+
+	length := binary.BigEndian.Uint16(header[0:2])
+	frameType := header[2]
+
+	encryptedPayload := make([]byte, length)
+	if _, err := io.ReadFull(reader, encryptedPayload); err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, 12)
+	binary.BigEndian.PutUint64(nonce[4:], s.readNonce)
+	s.readNonce++
+
+	payload, err := s.aead.Open(nil, nonce, encryptedPayload, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Frame{
+		Length:  length,
+		Type:    frameType,
+		Payload: payload,
+	}, nil
+}
+
+func (s *Session) WriteFrame(writer io.Writer, frameType uint8, data []byte) error {
+	nonce := make([]byte, 12)
+	binary.BigEndian.PutUint64(nonce[4:], s.writeNonce)
+	s.writeNonce++
+
+	encrypted := s.aead.Seal(nil, nonce, data, nil)
+
+	header := make([]byte, 3)
+	binary.BigEndian.PutUint16(header[0:2], uint16(len(encrypted)))
+	header[2] = frameType
+
+	if _, err := writer.Write(header); err != nil {
+		return err
+	}
+
+	if _, err := writer.Write(encrypted); err != nil {
+		return err
+	}
+
+	return nil
 }
