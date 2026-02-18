ddbcccc3a5714c1011be629d5891f0e7605e94e1
Parisa Mousavi <mousaviparisa5@yahoo.com>
2026-02-18 20:28:42 +0330

    feat: implement key generation and session key derivation

--- xray-core/proxy/reflex/handshake.go
+++ xray-core/proxy/reflex/handshake.go
@@ -1,5 +1,14 @@
 package reflex
 
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"io"
+
+	"golang.org/x/crypto/curve25519"
+	"golang.org/x/crypto/hkdf"
+)
+
 const ReflexMagic = 0x5246584C
 
 type ClientHandshake struct {
@@ -20,3 +29,28 @@ type ClientHandshakePacket struct {
 	Handshake ClientHandshake
 }
 
+func GenerateKeyPair() (privateKey [32]byte, publicKey [32]byte, err error) {
+	if _, err := io.ReadFull(rand.Reader, privateKey[:]); err != nil {
+		return privateKey, publicKey, err
+	}
+
+	privateKey[0] &= 248
+	privateKey[31] &= 127
+	privateKey[31] |= 64
+
+	curve25519.ScalarBaseMult(&publicKey, &privateKey)
+	return privateKey, publicKey, nil
+}
+
+func DeriveSharedKey(privateKey, peerPublicKey [32]byte) [32]byte {
+	var shared [32]byte
+	curve25519.ScalarMult(&shared, &privateKey, &peerPublicKey)
+	return shared
+}
+
+func DeriveSessionKey(sharedKey [32]byte, salt []byte) []byte {
+	h := hkdf.New(sha256.New, sharedKey[:], salt, []byte("reflex-session"))
+	sessionKey := make([]byte, 32)
+	io.ReadFull(h, sessionKey)
+	return sessionKey
+}
